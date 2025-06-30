490432f58dd6dd571a7e3b972ebe1d71d2e87337
ubaydillah1 <ubaydillah1737@gmail.com>
2025-06-30 12:44:41 +0700

    feat(appwrite): integrate appwrite authentication

---
+++ lib/appwrite/config.ts
@@ -0,0 +1,9 @@
+export const appwriteConfig = {
+  endpointUrl: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!,
+  projectId: process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!,
+  databaseId: process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID!,
+  userCollectionId: process.env.NEXT_PUBLIC_APPWRITE_USERS_COLLECTION!,
+  filesCollectionId: process.env.NEXT_PUBLIC_APPWRITE_FILES_COLLECTION!,
+  bucketId: process.env.NEXT_PUBLIC_APPWRITE_BUCKET!,
+  apiKey: process.env.NEXT_PUBLIC_APPWRITE_API_KEY!,
+};
