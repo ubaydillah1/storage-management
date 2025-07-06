760af339abd02f22af5c5dcfbd2db7bcae5c9da1
ubaydillah1 <ubaydillah1737@gmail.com>
2025-07-06 17:53:07 +0700

    fix: image issues

--- middleware.ts
+++ middleware.ts
@@ -21,5 +21,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|assets|favicon.ico).*)"],
 };
